5229221842c3735a94bd74fbdc81726359cede48
marcosgracioli <marvgm@gmail.com>
2020-02-26 09:40:40 -0300

    comitando ate terceiro video

---
+++ src/main/java/org/com/marcos/hibernate/Produto.java
@@ -0,0 +1,32 @@
+package org.com.marcos.hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+/**
+ * Produto
+ */
+@Entity
+public class Produto {
+
+    @Id
+    private Long id;
+
+    private String nome;
+
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+}
